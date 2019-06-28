e72fc07fd2ae5c22ddbca9b4ec05d2cdb7be7fbc
Daniel Vadil <dvadz@yahoo.com>
2019-06-27 20:29:27 -0400

    - added packages, front: axios, react-router-dome    backend: axios, mongoose - added database models

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: "email is required"
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        trim: true,
+        required: "password is required"
+    },
+    userName: {
+        type: String,
+        unique: true,
+        min: 3,
+        trim: true,
+        required: "firstName is required"
+    },
+    firstName: {
+        type: String,
+        trim: true,
+        required: "firstName is required"
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: "lastName is required"
+    }
+
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
