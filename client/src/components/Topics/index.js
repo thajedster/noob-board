535d48ff8b616d95f787789a295d5157d2f954b2
thajedster <jedlawrencecheng@gmail.com>
2019-06-29 19:17:01 -0400

    created topics component and styles as well as made minor changes on navbar and included new font in html

---
+++ client/src/components/Topics/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+export function Topics() {
+    return (
+        <div>
+            <div className="topics-title">
+                <h1>Hot Topics</h1>
+            </div>
+            <div className="top-topics">
+
+            </div>
+        </div>
+    )
+}
