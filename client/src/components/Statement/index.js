bd4094e10bd8b923b3d012b9810ce1d6099b7874
Malaka Alzoibi <malakaalzoibi@user3-237-213.wireless.utoronto.ca>
2019-07-06 10:59:09 -0400

    Bio and Statement folders created.

---
+++ client/src/components/Statement/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+function Statement() {
+    return (
+        <div className="card" id="statement">
+            <div className="card-body">
+                Welcome to our Noob-Board! Please share all your thpughts and questions that are related to programming! Our goal is to provide you with answers, knowledge and more ideas!
+            </div>
+        </div>
+    )
+}
+
+export default Statement;
+
+
+
+
+
