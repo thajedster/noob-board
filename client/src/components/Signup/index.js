6a50403b3466443339fc5069582d4d27a8ed551d
thajedster <jedlawrencecheng@gmail.com>
2019-07-06 12:13:12 -0400

    fixed minor errors and conflict

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -20,7 +20,6 @@ class Signup extends React.Component {
         });
     };
 
-<<<<<<< HEAD
     handleSubmit = event => {
         event.preventDefault();
         axios.post('/signup', {
@@ -39,11 +38,9 @@ class Signup extends React.Component {
                     lastName: ""
                 })
             )
-            .catch(err =>
-                ( if (err) throw err)
-            )
-
-
+            .catch(function (err) {
+                if (err) throw err;
+            })
     }
 
     render() {
@@ -78,62 +75,6 @@ class Signup extends React.Component {
             </div>
         )
     }
-=======
-  handleSubmit = event => {
-    event.preventDefault();
-    axios
-      .post("/signup", {
-        email: this.state.email,
-        password: this.state.password,
-        userName: this.state.userName,
-        firstName: this.state.firstName,
-        lastName: this.state.lastName
-      })
-      .then(function(res) {
-        console.log("new user created");
-      });
-  };
-
-  render() {
-    return (
-      <div className="row">
-        <div className="jumbotron jumbotron-fluid" id="sign">
-          <div className="container">
-            <h2 className="display-4">Sign Up Now!</h2>
-            <form>
-              <label>
-                First Name:
-                <input type="text" name="firstName" onChange={this.handleChange} value={this.state.firstName} />
-              </label>
-              <br />
-              <label>
-                Last Name:
-                <input type="text" name="lastName" onChange={this.handleChange} value={this.state.lastName} />
-              </label>
-              <br />
-              <label>
-                Username:
-                <input type="text" name="userName" onChange={this.handleChange} value={this.state.userName} />
-              </label>
-              <br />
-              <label>
-                E-mail:
-                <input type="text" name="email" onChange={this.handleChange} value={this.state.email} />
-              </label>
-              <br />
-              <label>
-                Password:
-                <input type="password" name="password" onChange={this.handleChange} value={this.state.password} />
-              </label>
-              <br />
-              <input type="submit" value="Submit" onClick={this.handleSubmit} />
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
->>>>>>> 09bc308280567ecd180d715938a76968adfb0344
 }
 
 export default Signup;
