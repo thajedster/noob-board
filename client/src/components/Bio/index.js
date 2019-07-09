3f4e2907eb48104c85be3ee0ecf1420774466755
thajedster <jedlawrencecheng@gmail.com>
2019-07-08 20:11:33 -0400

    added todo comments and axios call to topics container

--- client/src/components/Bio/index.js
+++ client/src/components/Bio/index.js
@@ -1,64 +1,46 @@
-import React from 'react';
-import './style.css';
-import axios from 'axios'; 
-import logo from '../Navbar/noob-logo.png';
-
+import React from "react";
+import "./style.css";
+import axios from "axios";
+import logo from "../Navbar/noob-logo.png";
 
 class Bio extends React.Component {
-    state = {
-        name:"",
-        userName: "",
-        email: ""
-    }
+  state = {
+    name: "",
+    userName: "",
+    email: ""
+  };
 
-    componentWillMount(){
-        axios.get("/api/user").then(
-            function(response) {
-            
-                const name = response.data.firstName + response.data.lastName 
-                this.setState({name}); 
+  componentWillMount() {
+    axios.get("/api/user").then(response => {
+      const name = response.data.firstName + response.data.lastName;
+      this.setState({ name });
 
-                const userName = response.data.userName
-                this.setState({userName}); 
+      const userName = response.data.userName;
+      this.setState({ userName });
 
-                const email = response.data.email
-                this.setState({email}); 
-        
+      const email = response.data.email;
+      this.setState({ email });
 
-              console.log("Name: " + response.data.firstName + response.data.lastName);
-              console.log("User Name: " + response.data.userName);
-              console.log("Email: " + response.data.email);
-            }
-        );
-    };
+      console.log("Name: " + response.data.firstName + response.data.lastName);
+      console.log("User Name: " + response.data.userName);
+      console.log("Email: " + response.data.email);
+    });
+  }
 
-
-    render(){
-        return (
-            <div className="bio">
-                <div className="card">
-                    <img src={logo} className="card-img-top" alt='logo'/>
-                <div className="card-body">
-                    <p className="card-text">Name:{this.state.name}</p>
-                    <p className="card-text">User Name:{this.state.userName}</p>
-                    <p className="card-text">Email:{this.state.email}</p>
-                </div>
-                </div>
-            </div>
-        )
-    }
-    
+  render() {
+    return (
+      <div className='bio'>
+        <div className='card'>
+          <img src={logo} className='card-img-top' alt='logo' />
+          <div className='card-body'>
+            <p className='card-text'>Name:{this.state.name}</p>
+            <p className='card-text'>User Name:{this.state.userName}</p>
+            <p className='card-text'>Email:{this.state.email}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Bio;
-
-
-
-
-
-
-
-
-
-
-
