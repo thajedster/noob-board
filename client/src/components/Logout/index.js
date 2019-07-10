9aa3ac4021c727e2f8c93288c538396ec59ddec0
Chris Armata <chris.armata@gmail.com>
2019-07-09 20:25:12 -0400

    Add initial logout component

---
+++ client/src/components/Logout/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import auth from "../../utils/auth";
+
+class Logout extends Component {
+  handleClick = e => {
+    auth
+      .logout()
+      .then(res => {
+        console.log("Logged out");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <button className="btn btn-primary ml-2" onClick={this.handleClick}>
+        Sign Out
+      </button>
+    );
+  }
+}
+
+export default Logout;
