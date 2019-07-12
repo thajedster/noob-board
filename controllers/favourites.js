42e073b4cb9871c1d5a12a3df01ed9e627373ce5
Chris Armata <chris.armata@gmail.com>
2019-07-12 18:08:32 -0400

    Add favourites controller

---
+++ controllers/favourites.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+
+exports.update = (req, res) => {
+  User.findOne({ _id: req.body.userId })
+    .select("favourites")
+    .then(userData => {
+      let op;
+      if (userData.favourites.includes(req.body.postId)) {
+        op = "$pull";
+      } else {
+        op = "$push";
+      }
+      User.findOneAndUpdate(
+        { _id: req.body.userId },
+        {
+          [op]: {
+            favourites: req.body.postId
+          }
+        },
+        { new: true }
+      )
+        .then(data => {
+          if (!data) {
+            res.status(500).end();
+          } else {
+            res.json({ favourites: data.favourites });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          res
+            .status(500)
+            .json(err)
+            .end();
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json(err)
+        .end();
+    });
+};
