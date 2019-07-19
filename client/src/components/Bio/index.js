3c1b195ebfba04b48fa402fb061f8219e660870e
Chris Armata <chris.armata@gmail.com>
2019-07-19 18:51:10 -0400

    Style profile

--- client/src/components/Bio/index.js
+++ client/src/components/Bio/index.js
@@ -46,18 +46,26 @@ class Bio extends Component {
   //store res data in array in state
   //after render, create link to
   render() {
-    const { name, userName, email } = this.state;
+    const { name, userName } = this.state;
     return (
-      <div className="bio">
-        <div className="card">
-          <img width={100} height={100} src={avatar} className="avatar" alt="" />
-          <div className="card-body">
-            <p className="card-text">Name: {name}</p>
-            <p className="card-text">User Name: {userName}</p>
-            <p className="card-text">Email: {email}</p>
+      <div className="row pt-3">
+        <div className="col-12 col-sm-8 col-md-6 col-lg-4 mx-auto">
+          <div id="bio" className="card mb-3">
+            <div className="row no-gutters">
+              <div className="col-4">
+                <img width={100} height={100} src={avatar} className="avatar" alt="" />
+              </div>
+              <div className="col-8">
+                <div className="card-body">
+                  <h5 className="card-text">{name}</h5>
+                  <h6 className="card-text text-muted">@{userName}</h6>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="userPosts">
-            My Posts: <Suggestions results={this.state.results} />
+          <div id="userPosts" className="card mb-3">
+            <div className="card-header">Your Posts</div>
+            <Suggestions results={this.state.results} />
           </div>
         </div>
       </div>
